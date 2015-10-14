8c068c090505a90c04c4abc2256476dd0162d760
olegrok <babinoleg@mail.ru>
2015-10-15 01:42:57 +0300

    Update myLS.c

--- myLS.c
+++ myLS.c
@@ -12,29 +12,43 @@
 #include <pwd.h>
 
 
-char* bits_to_rwx(int bits)
+char *bits_to_rwx(int bits)
 {
-    int bit = 0;
-    int i = 0;
-    for(; i < 3; i++)
-    {
-        bit = (bits & 0777) & (7 * (int)pow(8, 2 - i));
-        bit >>= 3 * (2 - i);
-        switch(bit)
-        {
-            case 0: printf("---");break;
-            case 1: printf("--x");break;
-            case 2: printf("-w-");break;
-            case 3: printf("-wx");break;
-            case 4: printf("r--");break;
-            case 5: printf("r-x");break;
-            case 6: printf("rw-");break;
-            case 7: printf("rwx");break;
+	int bit = 0;
+	int i = 0;
+	for (; i < 3; i++) {
+		bit = (bits & 0777) & (7 * (int) pow(8, 2 - i));
+		bit >>= 3 * (2 - i);
+		switch (bit) {
+		case 0:
+			printf("---");
+			break;
+		case 1:
+			printf("--x");
+			break;
+		case 2:
+			printf("-w-");
+			break;
+		case 3:
+			printf("-wx");
+			break;
+		case 4:
+			printf("r--");
+			break;
+		case 5:
+			printf("r-x");
+			break;
+		case 6:
+			printf("rw-");
+			break;
+		case 7:
+			printf("rwx");
+			break;
 
-        }
-    }
-    printf("   ");
-    return NULL;
+		}
+	}
+	printf("   ");
+	return NULL;
 
 }
 
@@ -105,37 +119,34 @@ char *concat_path(char *path, char *name)
 
 }
 
-void info(char* path, char name[], int flag, unsigned char type)
-{	
+void info(char *path, char name[], int flag, unsigned char type)
+{
 	struct stat ms = { };
-	struct dirent *curr_dir =
-	    (struct dirent *) calloc(1, sizeof(struct dirent));
 	stat(path, &ms);
-	
-	if(flag & 2)
-		printf("%d   ", (int)ms.st_ino);
-	if(type == DT_DIR)
-		printf("d");
-	else
-		printf("-");
-	bits_to_rwx(ms.st_mode);
-	if(flag & 8){	
-		printf("%d  ", ms.st_uid);
-		printf("%d  ", ms.st_gid);
+	if (flag & 4 || flag & 8) {
+		if (flag & 2)
+			printf("%d   ", (int) ms.st_ino);
+		if (type == DT_DIR)
+			printf("d");
+		else
+			printf("-");
+		bits_to_rwx(ms.st_mode);
+		if (flag & 8) {
+			printf("%d  ", ms.st_uid);
+			printf("%d  ", ms.st_gid);
+		} else {
+			struct group *grp = 0;
+			struct passwd *usr = 0;
+			grp = getgrgid(ms.st_gid);
+			usr = getpwuid(ms.st_uid);
+			printf("%s  ", usr->pw_name);
+			printf("%s  ", grp->gr_name);
+		}
+		char *time = ctime(&ms.st_mtime);
+		time[strlen(time) - 1] = 0;
+		printf("%s   ", time);
 	}
-	else
-	{
-		struct group* grp = 0;
-		struct passwd* usr = 0;
-		grp = getgrgid(ms.st_gid);
-		usr = getpwuid(ms.st_uid);
-		printf("%s  ", usr -> pw_name);
-		printf("%s  ", grp -> gr_name);
-	}
-	printf("%s  ", name);
-	printf("%s", ctime(&ms.st_mtime));
-	
-	free(curr_dir);
+	printf("%s\n", name);
 }
 
 
@@ -144,28 +155,28 @@ void directory_parse(char *path, int flags)
 	int len = 0;
 	DIR *dr = opendir(path);
 	char *buf = (char *) calloc(sizeof(char), 256);
-	struct dirent *curr_dir =
-	    (struct dirent *) calloc(1, sizeof(struct dirent));
+	struct dirent *curr_dir = 0;
 	strcpy(buf, path);
 	while (curr_dir = readdir(dr)) {
+		if (curr_dir->d_name[0] == '.' && !(flags & 1))
+			continue;
 		if (curr_dir->d_type == DT_DIR) {
-			printf("DIR: %s\n", curr_dir->d_name);
-			info(path, curr_dir->d_name, flags, curr_dir->d_type);
+			info(path, curr_dir->d_name, flags,
+			     curr_dir->d_type);
 			len = strlen(path);
 			path = concat_path(path, curr_dir->d_name);
-			if (curr_dir->d_name[0] != '.') {
-				if(flags & 16){
-					printf("\nI'm in DIR %s\n",
-				       curr_dir->d_name);
+			if (strcmp(curr_dir->d_name, ".") && strcmp(curr_dir->d_name, "..")) {
+				if (flags & 16) {
+					printf("\n%s:\n", path);
 					directory_parse(path, flags);
-					printf("\nExit  %s\n", curr_dir->d_name);
+					printf("\n");
 				}
 			}
 			path[len] = 0;
-		} else
-			{
-				info(path, curr_dir->d_name, flags, curr_dir->d_type);
-			}
+		} else {
+			info(path, curr_dir->d_name, flags,
+			     curr_dir->d_type);
+		}
 
 
 	}
@@ -174,16 +185,9 @@ void directory_parse(char *path, int flags)
 	free(curr_dir);
 }
 
-
-
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *env[])
 {
-	if(argc < 2)
-	{
-		printf("There is no arguments\n");
-		return 0;
-	}
+
 
 	int flags = 0;
 	flags = arguments_parsing(argc, argv);
@@ -191,36 +195,17 @@ int main(int argc, char *argv[])
 	struct stat ms = { };
 	struct dirent *curr_dir =
 	    (struct dirent *) calloc(1, sizeof(struct dirent));
-	printf("Name: %s\n", argv[argc - 1]);
-	stat(argv[argc - 1], &ms);
-	printf("Inode number: %d\n", (int)ms.st_ino);
-	printf("UID: %d\n", ms.st_uid);
-	printf("GID %d\n", ms.st_gid);
-	printf("Protection %o\n", ms.st_mode & 0777);
-	
-	//for_time = ctime(&ms.st_atim);
-	printf("Last status change:       %s", ctime(&ms.st_ctime));
-        printf("Last file access:         %s", ctime(&ms.st_atime));
-        printf("Last file modification:   %s", ctime(&ms.st_mtime));
-
-
+	if (argv[argc - 1][0] == '-' || argc < 2) {
+		argv[argc] = getenv("PWD");
+		argc++;
+		argv[argc] = 0;
+	}
 	char *buf = (char *) calloc(sizeof(char), 256);
 	strcpy(buf, argv[argc - 1]);
-	//DIR* dr = opendir(argv[1]);
-
-
-	/*while(curr_dir = readdir(dr))
-	   {
-	   printf("%s\n", curr_dir -> d_name);
-	   }
-	 */
-
 	directory_parse(buf, flags);
 
 
 	free(buf);
-
 	free(curr_dir);
-//      closedir(dr);
 	return 0;
 }
