62b6a509ce5a6bf39c1a5f75683a27bff6c0cb29
olegrok <babinoleg@mail.ru>
2015-10-22 12:36:24 +0300

    Update mysh+pipe.c

--- mysh+pipe.c
+++ mysh+pipe.c
@@ -16,21 +16,23 @@ int exit_check(const char *str)
 {
 	int len = strlen(str);
 	int i = 0;
-	for(;i < len; i++){
-		if(isgraph(str[i]))
-			break;}
-	if(str[i - 1] == '\n')
-		return EMPTYSTR;
-	if(str[i] == '|'){
-		printf("mybash: ошибка синтаксиса около неожиданной лексемы `|'\n");
-		return EMPTYSTR;	
+	for (; i < len; i++) {
+		if (isgraph(str[i]))
+			break;
 	}
-	if(i < len){
-		if(!strncmp(&str[i], "myexit", 6) && !isgraph(str[i + 6]))
+	if (str[i - 1] == '\n')
+		return EMPTYSTR;
+	if (str[i] == '|') {
+		printf
+		    ("mybash: ошибка синтаксиса около неожиданной лексемы `|'\n");
+		return EMPTYSTR;
+	}
+	if (i < len) {
+		if (!strncmp(&str[i], "myexit", 6) && !isgraph(str[i + 6]))
 			return -1;
-		if(i == len)
+		if (i == len)
 			return EMPTYSTR;
-		}
+	}
 	return 0;
 }
 
@@ -55,86 +57,82 @@ int parse(char *str, char *arg[], const char *delim)
 
 int main()
 {
-	char str[size] = { };	
+	char str[size] = { };
 	char *args[size] = { };
 	char *parts_array[size] = { };
 	int str_ok = 0, parts = 0, i = 0, for_fork = 0;
 	int pipefd1[2] = { };
 	int pipefd2[2] = { };
-	char* for_fgets = 0;
+	char *for_fgets = 0;
 	while (1) {
 		printf("myshell $ ");
 		for_fgets = fgets(str, size, stdin);
-		if(for_fgets == NULL)
-			{
-				if(errno == EIO)
-					return -1;
-				return 0;
-			}
+		if (for_fgets == NULL) {
+			if (errno == EIO)
+				return -1;
+			return 0;
+		}
 		str_ok = exit_check(str);
 		if (str_ok == EMPTYSTR)
-			continue;     
+			continue;
 		if (str_ok == -1)
-			break;		
-		parts = parse(str, parts_array,"|");		
+			break;
+		parts = parse(str, parts_array, "|");
 		for (i = 0; i < parts; i++) {
-			if(pipe(pipefd1) < 0)
+			if (pipe(pipefd1) < 0)
 				perror("Pipe failed");
 			for_fork = fork();
-			if(for_fork < 0)
-			{
+			if (for_fork < 0) {
 				perror("Fork failed");
 				return -1;
 			}
 			if (for_fork == 0) {
-				if(close(pipefd1[0]) < 0){
+				if (close(pipefd1[0]) < 0) {
 					perror("Close failed");
 					exit(-1);
 				}
 				parse(parts_array[i], args, " \n");
-				if (i != 0) {										
-					if(dup2(pipefd2[0], 0) < 0){
-						perror("Dup2 failed");											
-						exit(-1);
-					}	
-				}
-				if (i != (parts - 1)){
-					if(dup2(pipefd1[1], 1) < 0){
-						perror("Dup2 failed");											
+				if (i != 0) {
+					if (dup2(pipefd2[0], 0) < 0) {
+						perror("Dup2 failed");
 						exit(-1);
 					}
 				}
-				else
-					if(close(pipefd1[1]) < 0){
-						perror("Close failed");											
+				if (i != (parts - 1)) {
+					if (dup2(pipefd1[1], 1) < 0) {
+						perror("Dup2 failed");
 						exit(-1);
-					
-					}				
+					}
+				} else if (close(pipefd1[1]) < 0) {
+					perror("Close failed");
+					exit(-1);
+
+				}
 				execvp(args[0], args);
 				perror("Exec");
 				return -1;
 			}
 
-			if(close(pipefd1[1]) < 0){
-					perror("Close failed");											
-				}
-			if (i != 0)
-				if(close(pipefd2[0]) < 0){
-					perror("Close failed");											
-				}
-			if(pipe(pipefd2) < 0)
-				perror("Pipe failed");											
-			if(close(pipefd2[1]) < 0)
-				perror("Close failed"); 												
-			if(dup2(pipefd1[0], pipefd2[0]) < 0)
-				perror("Dup2 failed");											
-			if(close(pipefd1[0]) < 0)
+			if (close(pipefd1[1]) < 0) {
 				perror("Close failed");
-			if (i == (parts - 1)){
-				if(close(pipefd2[0]) < 0)
-					perror("Close failed");											
+			}
+			if (i != 0)
+				if (close(pipefd2[0]) < 0) {
+					perror("Close failed");
 				}
-			if(errno == EBADF)
+			if (pipe(pipefd2) < 0)
+				perror("Pipe failed");
+			if (close(pipefd2[1]) < 0)
+				perror("Close failed");
+			if (dup2(pipefd1[0], pipefd2[0]) < 0)
+				perror("Dup2 failed");
+			if (close(pipefd1[0]) < 0)
+				perror("Close failed");
+			if (i == (parts - 1)) {
+				if (close(pipefd2[0]) < 0)
+					perror("Close failed");
+			}
+			if (errno == EBADF)
 				break;
 		}
 
